057e35bb5d7148cbabb958f0d30409ac9887e7a9
menachem margolis <margolismendel5728@gmail.com>
2021-02-09 16:17:59 -0500

    commit tree 06c371e51d8bba39da3b5a2182eb9e713f6aadb2

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Add your code here
+
+const url = 'http://localhost:3000/users'
+function submitData(name, email){
+
+  obj ={name: name, email: email}
+  
+  let headersAndBody = {
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify(obj) 
+  }
+  
+  fetch(url, headersAndBody)
+  .then(response => response.json())
+  .then(data => console.log(data))
+
+}
+
+submitData('mendel','surmendel@gmail.com')
